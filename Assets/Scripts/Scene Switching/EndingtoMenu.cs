3ceb571d08b44e51926cd5e7525a4757830ab010
JanAdell <36292051+JanAdell@users.noreply.github.com>
2023-04-11 14:23:47 +0200

    Added full scene connectivity

---
+++ Assets/Scripts/Scene Switching/EndingtoMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingtoMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.fullScreen = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+}
