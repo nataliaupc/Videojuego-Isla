7ddc3b921eab439a759eb2b505b579a22b904e0d
JanAdell <36292051+JanAdell@users.noreply.github.com>
2023-06-04 00:49:42 +0200

    Added SFX where needed (desc) and some QOL on player movement
    SFX For:
    -Player Footsteps
    -Sea Ambience
    -Jungle Sounds
    -Fire Sound

---
+++ Assets/Scripts/Interaction/Footsteps.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+
+    public AudioSource footsteps;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            footsteps.enabled = true;
+        }
+        else
+            footsteps.enabled = false;
+    }
+}
